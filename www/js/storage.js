1ea631aa1e0f65d6177557801cb1c287123e4e90
obook <obooklage@free.fr>
2024-04-13 13:01:46 +0200

    Update storage.js

--- www/js/storage.js
+++ www/js/storage.js
@@ -4,8 +4,8 @@
 *
 */
 
-import { QuestionObj, GetCurrentQuestion, SetCurrentQuestion} from "./question.js";
-export { StorageExists, StorageClear};
+
+export { StorageAvailable, StorageExists, StorageClear};
 
 function StorageClear() {
   localStorage.clear();
@@ -14,13 +14,41 @@ function StorageClear() {
 function StorageExists(number) {
   let key = "Q"+ number;
 
-  try {
-    if (localStorage.getItem(key))
-      return(true);
-  } catch (error) {
-      console.log("StorageExists error trig !");
+  if (StorageAvailable("localStorage")) {
+    try {
+      if (localStorage.getItem(key))
+        return(true);
+    } catch (error) {
+        console.log("StorageExists error trig !");
+    }
+  } else {
+    // Malheureusement, localStorage n'est pas disponible
   }
 
-  // console.log("StorageExists "+number+" :False");
 return(false);
+}
+
+function StorageAvailable(type) {
+  try {
+    var storage = window[type],
+      x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage.length !== 0
+    );
+  }
 }
